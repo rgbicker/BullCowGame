2e6165e6fa3c65979c1be8194f989f01c5bc2c93
ryan bicker <rgbicker@yahoo.com>
2017-08-28 19:29:45 -0400

    Added introduction to the game, and input and output of guesses.

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+void PrintIntro();
+string GetAndPrintGuess();
 
 
+// the entry point for our application
 int main()
 {
+	PrintIntro();
+
+	constexpr int GuessTimes = 4;
+	for (int count = 0; count <= GuessTimes; count++)
+	{
+		GetAndPrintGuess();
+
+	}
 
 	return 0;
+}
+
+// introduce the game
+void PrintIntro()
+{
+
+	constexpr int WORD_LENGTH = 5;
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout << " letter isogram (a word with no repeating letters) I'm thinking of?\n";
+	cout << endl;
+	return;
+}
+
+// get a guess from the player and print it back
+string GetAndPrintGuess()
+{
+
+	cout << "Enter your guess: ";
+	string Guess = "";
+	getline(cin, Guess);
+
+	cout << "Your guess was: " << Guess << " \n";
+	cout << endl;
+	return Guess;
 }
